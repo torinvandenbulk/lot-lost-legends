552df5a1d31f335d6b141ac9becb5cd8dbbb3939
torinvandenbulk <65670557+torinvandenbulk@users.noreply.github.com>
2023-05-19 18:34:09 -0400

    Update character-class.cs

--- assets/scripts/character/character-class.cs
+++ assets/scripts/character/character-class.cs
@@ -10,19 +10,7 @@ public enum CharacterClass
     Rogue
 }
 
-[System.Serializable]
-public class HeroAttributes
-{
-    public int strength;
-    public int dexterity;
-    public int constitution;
-    public int intelligence;
-    public int wisdom;
-    public int charisma;
-}
-
 public class CharacterClassData : MonoBehaviour
 {
     public CharacterClass characterClass;
-    public HeroAttributes attributes;
 }
