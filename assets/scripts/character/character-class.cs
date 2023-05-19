e8abca972e3da8319bd6133975effb2304c34a9a
torinvandenbulk <65670557+torinvandenbulk@users.noreply.github.com>
2023-05-19 18:37:59 -0400

    Update character-class.cs

--- assets/scripts/character/character-class.cs
+++ assets/scripts/character/character-class.cs
@@ -13,4 +13,61 @@ public enum CharacterClass
 public class CharacterClassData : MonoBehaviour
 {
     public CharacterClass characterClass;
+
+    public CharacterClassData(CharacterClass characterClass)
+    {
+        this.characterClass = characterClass;
+    }
+
+    public string GetCharacterClassAsString()
+    {
+        switch (characterClass)
+        {
+            case CharacterClass.Cleric:
+                return "Cleric";
+            case CharacterClass.Paladin:
+                return "Paladin";
+            case CharacterClass.Fighter:
+                return "Fighter";
+            case CharacterClass.Sorcerer:
+                return "Sorcerer";
+            case CharacterClass.Barbarian:
+                return "Barbarian";
+            case CharacterClass.Rogue:
+                return "Rogue";
+            default:
+                return null;
+        }
+    }
+
+    public void ApplyBonuses()
+    {
+        switch (characterClass)
+        {
+            case CharacterClass.Cleric:
+                Strength += 1;
+                Wisdom += 2;
+                break;
+            case CharacterClass.Paladin:
+                Strength += 2;
+                Charisma += 2;
+                break;
+            case CharacterClass.Fighter:
+                Strength += 2;
+                Constitution += 1;
+                break;
+            case CharacterClass.Sorcerer:
+                Charisma += 2;
+                Intelligence += 1;
+                break;
+            case CharacterClass.Barbarian:
+                Strength += 3;
+                Constitution += 2;
+                break;
+            case CharacterClass.Rogue:
+                Dexterity += 2;
+                Intelligence += 1;
+                break;
+        }
+    }
 }
