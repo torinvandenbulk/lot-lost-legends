4816a5b1dab9069b05e0566e2d1bc126a62f6c51
torinvandenbulk <65670557+torinvandenbulk@users.noreply.github.com>
2023-05-19 19:28:18 -0400

    Create armor.cs

---
+++ assets/scripts/items/armor.cs
@@ -0,0 +1,26 @@
+public class Armor
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int AC { get; set; }
+
+    public Armor()
+    {
+        Name = "";
+        Description = "";
+        AC = 0;
+    }
+
+    public Armor(string name, string description, int AC)
+        : this()
+    {
+        Name = name;
+        Description = description;
+        AC = AC;
+    }
+}
+
+// List of basic D&D starting armor
+
+public static Armor Chainmail = new Armor("Chainmail", "A type of armor that provides an AC of 16.", 16);
+public static Armor Plate = new Armor("Plate", "A type of armor that provides an AC of 18.", 18);
