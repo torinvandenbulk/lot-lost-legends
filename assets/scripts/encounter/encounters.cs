47b353b876d5651978c60eec7d901cb779b0b2fa
torinvandenbulk <65670557+torinvandenbulk@users.noreply.github.com>
2023-05-19 22:20:28 -0400

    Update encounters.cs

--- assets/scripts/encounter/encounters.cs
+++ assets/scripts/encounter/encounters.cs
@@ -1,35 +1,138 @@
-public class Encounters
-{
-    public static List<Encounter> EncountersList { get; set; } = new List<Encounter>();
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-    public Encounters()
+namespace Encounters
+{
+    public class Encounters
     {
-        EncountersList.Add(new Encounter("Bandits", 1, 1));
-        EncountersList.Add(new Encounter("Goblins", 2, 2));
-        EncountersList.Add(new Encounter("Orcs", 3, 3));
-        EncountersList.Add(new Encounter("Trolls", 4, 4));
+        public static void GenerateEncounter(int level, DungeonPath path)
+        {
+            // Randomly select a biome
+            var biome = Enumerable.Range(0, 100).Select(x => (Biome)x).Random();
+
+            // Get the list of NPCs for the biome
+            var npcs = GetNPCs(biome);
+
+            // Randomly select a number of NPCs
+            var numNPCs = Enumerable.Range(1, 10).Select(x => x).Random();
+
+            // Create a list of the NPCs for the encounter
+            var encounter = new List<NPC>();
+            for (int i = 0; i < numNPCs; i++)
+            {
+                encounter.Add(npcs[i]);
+            }
+
+            // Calculate the challenge rating of the encounter
+            var cr = CalculateCR(encounter);
+
+            // Get the next location on the dungeon path
+            var nextLocation = path.GetNextLocation();
+
+            // Add the encounter to the dungeon path
+            path.AddEncounter(nextLocation, encounter);
+
+            // Print the encounter details
+            Console.WriteLine("Encounter:");
+            Console.WriteLine("Biome: " + biome);
+            Console.WriteLine("Number of NPCs: " + numNPCs);
+            Console.WriteLine("Challenge Rating: " + cr);
+
+            // Print the details of each NPC
+            foreach (var npc in encounter)
+            {
+                Console.WriteLine(npc);
+            }
+        }
+
+        private static List<NPC> GetNPCs(Biome biome)
+        {
+            // Load the NPC data
+            var npcData = JsonConvert.DeserializeObject<List<NPC>>(File.ReadAllText("npc.json"));
+
+            // Get the list of NPCs for the biome
+            var filteredNPCs = npcData.Where(npc => npc.Biome == biome).ToList();
+
+            // Return the list of NPCs
+            return filteredNPCs;
+        }
+
+        private static int CalculateCR(List<NPC> encounter)
+        {
+            // Calculate the total challenge rating of the encounter
+            int cr = 0;
+            foreach (var npc in encounter)
+            {
+                cr += npc.CR;
+            }
+
+            // Return the challenge rating
+            return cr;
+        }
     }
 
-    public static Encounter GetEncounter(int dungeonPathLength)
+    public enum Biome
     {
-        // Generate a random number between 1 and the length of the dungeon path.
-        int randomNumber = Convert.ToInt32(Math.Round(Math.Random() * dungeonPathLength));
-
-        // Return the encounter at the specified index.
-        return EncountersList[randomNumber];
+        Forest,
+        Desert,
+        Mountain,
+        Swamp,
+        Tundra
     }
-}
 
-public class Encounter
-{
-    public string Name { get; set; }
-    public int Number { get; set; }
-    public int Difficulty { get; set; }
-
-    public Encounter(string name, int number, int difficulty)
+    public class NPC
     {
-        Name = name;
-        Number = number;
-        Difficulty = difficulty;
+        public string Name { get; set; }
+        public Biome Biome { get; set; }
+        public int CR { get; set; }
+    }
+
+    public class DungeonPath
+    {
+        public List<Location> Locations { get; set; }
+
+        public DungeonPath()
+        {
+            Locations = new List<Location>();
+        }
+
+        public void AddLocation(Location location)
+        {
+            Locations.Add(location);
+        }
+
+        public Location GetNextLocation()
+        {
+            return Locations[0];
+        }
+    }
+
+    public class Location
+    {
+        public string Name { get; set; }
+        public List<Encounter> Encounters { get; set; }
+
+        public Location(string name)
+        {
+            Name = name;
+            Encounters = new List<Encounter>();
+        }
+
+        public void AddEncounter(Encounter encounter)
+        {
+            Encounters.Add(encounter);
+        }
+    }
+
+    public class Encounter
+    {
+        public List<NPC> NPCs { get; set; }
+
+        public Encounter(List<NPC> npcs)
+        {
+            NPCs = npcs;
+        }
     }
 }
