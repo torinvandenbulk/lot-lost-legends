7a0510da2876e3fb3219e23a14c1be094cc415ff
torinvandenbulk <65670557+torinvandenbulk@users.noreply.github.com>
2023-05-19 22:37:24 -0400

    Update combat-manager.cs

--- assets/scripts/combat/combat-manager.cs
+++ assets/scripts/combat/combat-manager.cs
@@ -1,38 +1,81 @@
+using System;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Linq;
+using System.Text;
 
-public class CombatManager : MonoBehaviour
+namespace CombatManager
 {
-    public List<Character> charactersInCombat;
-
-    private void Start()
+    public class CombatManager
     {
-        StartCombat();
-    }
-
-    public void StartCombat()
-    {
-        foreach (Character character in charactersInCombat)
+        public static void StartCombat(Encounter encounter, DungeonPath path)
         {
-            character.RollInitiative();
+            // Get the list of NPCs for the encounter
+            var npcs = encounter.NPCs;
+
+            // Roll initiative for each NPC
+            foreach (var npc in npcs)
+            {
+                npc.Initiative = Random.Next(20);
+            }
+
+            // Sort the NPCs by initiative
+            npcs.Sort((a, b) => a.Initiative - b.Initiative);
+
+            // Print the encounter details
+            Console.WriteLine("Encounter:");
+            Console.WriteLine("Biome: " + encounter.Biome);
+            Console.WriteLine("Number of NPCs: " + npcs.Count);
+            Console.WriteLine("Challenge Rating: " + encounter.CR);
+
+            // Print the details of each NPC
+            foreach (var npc in npcs)
+            {
+                Console.WriteLine(npc);
+            }
+
+            // Start the combat loop
+            RunCombat(npcs, path);
         }
 
-        charactersInCombat.Sort((c1, c2) => c2.initiative.CompareTo(c1.initiative));
-        StartCoroutine(ExecuteTurns());
-    }
-
-    private IEnumerator ExecuteTurns()
-    {
-        while (true)
+        private static void RunCombat(List<NPC> npcs, DungeonPath path)
         {
-            foreach (Character character in charactersInCombat)
+            // Loop until all NPCs are dead or the players are dead
+            while (npcs.Any(npc => npc.IsAlive) && path.IsAlive)
             {
-                // Execute character's turn
-                Debug.Log($"{character.characterName} takes their turn.");
+                // Get the next NPC to act
+                var npc = npcs.FirstOrDefault(npc => npc.Initiative == 0);
 
-                // Placeholder for the actual turn execution
-                yield return new WaitForSeconds(1);
+                // If there is no next NPC, then the players have a turn
+                if (npc == null)
+                {
+                    // Have the players take their turn
+                    path.TakeTurn();
+                }
+                else
+                {
+                    // Have the NPC take their turn
+                    npc.TakeTurn(path);
+                }
+
+                // Decrement the initiative of all NPCs
+                foreach (var npc in npcs)
+                {
+                    npc.Initiative--;
+                }
+            }
+
+            // Check if the players are dead
+            if (!path.IsAlive)
+            {
+                // The players have lost
+                Console.WriteLine("The players have lost!");
+            }
+            else
+            {
+                // The players have won
+                Console.WriteLine("The players have won!");
             }
         }
     }
 }
+
