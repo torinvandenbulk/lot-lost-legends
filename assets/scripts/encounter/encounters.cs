0c655d3a52f57bf5b0819798359b75cfa96261d6
torinvandenbulk <65670557+torinvandenbulk@users.noreply.github.com>
2023-05-19 22:32:58 -0400

    Update encounters.cs

--- assets/scripts/encounter/encounters.cs
+++ assets/scripts/encounter/encounters.cs
@@ -9,67 +9,38 @@ namespace Encounters
     {
         public static void GenerateEncounter(int level, DungeonPath path)
         {
-            // Randomly select a biome
-            var biome = Enumerable.Range(0, 100).Select(x => (Biome)x).Random();
+            // Get the dungeon from the path
+            var dungeon = path.GetNextDungeon();
 
-            // Get the list of NPCs for the biome
-            var npcs = GetNPCs(biome);
-
-            // Randomly select a number of NPCs
-            var numNPCs = Enumerable.Range(1, 10).Select(x => x).Random();
-
-            // Create a list of the NPCs for the encounter
-            var encounter = new List<NPC>();
-            for (int i = 0; i < numNPCs; i++)
-            {
-                encounter.Add(npcs[i]);
-            }
-
-            // Calculate the challenge rating of the encounter
-            var cr = CalculateCR(encounter);
-
-            // Get the next location on the dungeon path
-            var nextLocation = path.GetNextLocation();
+            // Generate an encounter for the dungeon
+            var encounter = GenerateEncounter(dungeon.Level);
 
             // Add the encounter to the dungeon path
-            path.AddEncounter(nextLocation, encounter);
+            path.AddEncounter(dungeon, encounter);
 
             // Print the encounter details
             Console.WriteLine("Encounter:");
-            Console.WriteLine("Biome: " + biome);
-            Console.WriteLine("Number of NPCs: " + numNPCs);
-            Console.WriteLine("Challenge Rating: " + cr);
+            Console.WriteLine("Biome: " + dungeon.Biome);
+            Console.WriteLine("Number of NPCs: " + encounter.NPCs.Count);
+            Console.WriteLine("Challenge Rating: " + encounter.CR);
 
             // Print the details of each NPC
-            foreach (var npc in encounter)
+            foreach (var npc in encounter.NPCs)
             {
                 Console.WriteLine(npc);
             }
         }
 
-        private static List<NPC> GetNPCs(Biome biome)
+        private static Encounter GenerateEncounter(int level)
         {
-            // Load the NPC data
-            var npcData = JsonConvert.DeserializeObject<List<NPC>>(File.ReadAllText("npc.json"));
+            // Get the list of encounters for the level
+            var encounters = dungeon.Encounters.Where(encounter => encounter.Level <= level).ToList();
 
-            // Get the list of NPCs for the biome
-            var filteredNPCs = npcData.Where(npc => npc.Biome == biome).ToList();
+            // Randomly select an encounter
+            var encounter = encounters[Random.Next(encounters.Count)];
 
-            // Return the list of NPCs
-            return filteredNPCs;
-        }
-
-        private static int CalculateCR(List<NPC> encounter)
-        {
-            // Calculate the total challenge rating of the encounter
-            int cr = 0;
-            foreach (var npc in encounter)
-            {
-                cr += npc.CR;
-            }
-
-            // Return the challenge rating
-            return cr;
+            // Return the encounter
+            return encounter;
         }
     }
 
@@ -80,6 +51,40 @@ namespace Encounters
         Mountain,
         Swamp,
         Tundra
+    }
+
+    public class Dungeon
+    {
+        public string Name { get; set; }
+        public Biome Biome { get; set; }
+        public int Level { get; set; }
+        public int Length { get; set; }
+        public List<Encounter> Encounters { get; set; }
+
+        public Dungeon(string name, Biome biome, int level, int length)
+        {
+            Name = name;
+            Biome = biome;
+            Level = level;
+            Length = length;
+            Encounters = new List<Encounter>();
+        }
+
+        public void AddEncounter(Encounter encounter)
+        {
+            Encounters.Add(encounter);
+        }
+    }
+
+    public class Encounter
+    {
+        public List<NPC> NPCs { get; set; }
+        public int CR { get; set; }
+
+        public Encounter(List<NPC> npcs)
+        {
+            NPCs = npcs;
+        }
     }
 
     public class NPC
@@ -123,16 +128,6 @@ namespace Encounters
         public void AddEncounter(Encounter encounter)
         {
             Encounters.Add(encounter);
-        }
-    }
-
-    public class Encounter
-    {
-        public List<NPC> NPCs { get; set; }
-
-        public Encounter(List<NPC> npcs)
-        {
-            NPCs = npcs;
         }
     }
 }
